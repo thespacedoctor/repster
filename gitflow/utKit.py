d8d3426f79cd7ebf5cf473ecbc6c79eeef2b8f9f
thespacedoctor <nothingbutdave@gmail.com>
2014-06-04 15:34:48 +0100

    first commit

---
+++ gitflow/utKit.py
@@ -0,0 +1,15 @@
+from dryxPython.utKit import utKit
+
+## OVERRIDES
+
+
+class utKit(utKit):
+
+    """
+    Override dryx utKit
+    """
+    ## Variable Data Atrributes
+
+    ## Override Variable Data Atrributes
+    # x-dbConfig-tmpx
+
